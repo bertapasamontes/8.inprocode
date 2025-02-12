d70bd7e4991c9099117157b05025a69d9da81364
bertapasamontes <bertapasamontes@gmail.com>
2025-02-12 23:25:09 +0100

    nuevo proyecto + componentes creados + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { FullCalendarComponent } from './components/full-calendar/full-calendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: '',   redirectTo: '/home', pathMatch: 'full'},
+    { path: 'mapa', component: MapaComponent},
+    { path: 'calendar', component: FullCalendarComponent},
+    { path: 'charts', component: ChartsComponent},
+
+
+];
