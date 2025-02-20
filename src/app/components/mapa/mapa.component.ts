81c72d15fc774413c69ebaca14618f357e3bfccc
bertapasamontes <bertapasamontes@gmail.com>
2025-02-20 18:22:18 +0100

    buscador encuentra sitios en console log y se almacenan los sitios en array Places

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -3,10 +3,11 @@ import { PlacesService } from '../../services/mapa/places/places-service.service
 import { MapViewComponent } from "./components/map-view/map-view.component";
 import { ProgressBarComponent } from "../shared/progress-bar/progress-bar.component";
 import { BtnMyLocationComponent } from "./components/btn-my-location/btn-my-location.component";
+import { SearchBarComponent } from "./components/search-bar/search-bar.component";
 
 @Component({
     selector: 'app-mapa',
-    imports: [MapViewComponent, ProgressBarComponent, BtnMyLocationComponent],
+    imports: [MapViewComponent, ProgressBarComponent, BtnMyLocationComponent, SearchBarComponent],
     templateUrl: './mapa.component.html',
     styleUrl: './mapa.component.scss'
 })
--- src/app/services/mapa/places/places-service.service.ts
+++ src/app/services/mapa/places/places-service.service.ts
@@ -1,16 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../../../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
   userLocation?: [number, number];
+  isLoadingPlaces: boolean=false;
+  places: Feature[] = []; //feature en mapbox = los sitios.
 
   get isUserLocationReady(): boolean{
     return !!this.userLocation;
   }
 
-  constructor() {this.loadUserLocation(); }
+  constructor(
+    private http: HttpClient
+  ) {
+    this.loadUserLocation(); 
+  }
+
+
   private async loadUserLocation() {
     this.userLocation = await this.getUserLocation();
   }
@@ -28,5 +38,17 @@ export class PlacesService {
       }
     )
     })
+  }
+
+  getPlacesByQuery( query:string){
+
+    this.isLoadingPlaces = true;
+
+   this.http.get<PlacesResponse>(` https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=es&proximity=${this.userLocation?.[0]}%2C${this.userLocation?.[1]}&language=es&access_token=pk.eyJ1IjoiZG9udXRjb25jaG9jbyIsImEiOiJjbTdjM21qbm8wbDY2MmpzZXQ5bW11YTNtIn0.rQ9yW0c4WRaPaXCGs9gRdA`).subscribe( respuesta => {
+      console.log(respuesta.features);
+      this.isLoadingPlaces = false;
+      this.places=respuesta.features;
+
+    });
   }
 }
