b26483f5c3d2a25f98735a5a134a5c5db08bce5a
bertapasamontes <bertapasamontes@gmail.com>
2025-02-22 12:02:55 +0100

    calendario creado + eventos locales visibles. falta subirlos a mongo y que se puedan editar en un modal, eliminar, crear y editar

--- src/app/components/full-calendar/full-calendar.component.ts
+++ src/app/components/full-calendar/full-calendar.component.ts
@@ -6,7 +6,7 @@ import { CalendarOptions, EventInput } from '@fullcalendar/core'; // useful for 
 //plugins de FullCalendar
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-
+import esLocale from '@fullcalendar/core/locales/es'
 
 @Component({
     selector: 'app-full-calendar',
@@ -18,27 +18,42 @@ export class FullCalendarComponent {
 
     @ViewChild('calendario') calendarComponent?: FullCalendarComponent;
 
+
     calendarOptions: CalendarOptions = {
         initialView: 'dayGridMonth',
         plugins: [dayGridPlugin, interactionPlugin],
-        dateClick: (arg) => this.handleDateClick(arg),
-        events: [
-            { title: 'Meeting', start: new Date() }
-          ],
-        weekends: false // initial value
+        events:[],
+        locale: esLocale,
+        weekends: true, // findes visibles
     };
-    // eventsPromise?: Promise<EventInput[]>;
-
-    
-    handleDateClick(arg:any) {
-        alert('date click! ' + arg.dateStr)
-    }
     toggleWeekends() {
         this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
     }
 
-    someMethod() {
-        let calendarApi = this.calendarComponent.getApi();
-        calendarApi.next();
-      }
+    ngOnInit() {
+        this.calendarOptions.events=[
+            {
+                 id: '1', 
+                 title: 'Evento 1', 
+                 date: new Date(),
+                 description: "Evento 1"
+            },
+            { 
+                id: '2', 
+                 title: 'Evento 2', 
+                 date: new Date(new Date().getTime()+86400000), //coge la fecha de hoy y sumale un dia
+                 description: "Evento 2"
+            },
+            { 
+                id: '2', 
+                 title: 'Evento 3', 
+                 start: new Date(new Date().getTime()+86400000), //coge la fecha de hoy y sumale un dia
+                 end: new Date(new Date().getTime()+86400000*2),
+                 description: "Evento 2"
+            }
+        ]
+        console.log("eventos impresos");
+    }
+
+
 }
