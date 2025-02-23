1c6286d6e2cf19b679087f5e66ef8987c3ccfb7a
bertapasamontes <bertapasamontes@gmail.com>
2025-02-23 23:31:38 +0100

    chartjs instalados correctamente

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
     selector: 'app-charts',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
     styleUrl: './charts.component.scss'
 })
 export class ChartsComponent {
+    @ViewChild('chartCanvas') chartCanvas!: ElementRef;
+    constructor() {
+        Chart.register(...registerables); // 🔹 ¡Registra los componentes!
+      }
 
+    ngAfterViewInit() {
+        const ctx = this.chartCanvas.nativeElement.getContext('2d'); // Obtener el contexto
+    
+        if (ctx) {
+          new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: ['Enero', 'Febrero', 'Marzo'],
+              datasets: [
+                {
+                  label: 'Ventas',
+                  data: [100, 200, 150],
+                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              responsive: true,
+            },
+          });
+        }
+      }
 }
